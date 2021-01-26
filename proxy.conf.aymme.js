7f33d6fb35e40b28115a7d285f0f04f4310abdaf
jcoombs3 <coombsfilm@gmail.com>
2021-01-25 22:39:00 -0500

    Update proxy.conf.aymme.js

--- proxy.conf.aymme.js
+++ proxy.conf.aymme.js
@@ -1,20 +1,42 @@
+const PROJECT_NAME = "bbus-content-app";
 const PROXY_CONFIG = {
-	"/api": {
-		"target": "http://0.0.0.0:7777/api",
+	"/api/contentservices": {
+		"target": "http://0.0.0.0:7777/api/contentservices",
 		"secure": false,
 		"changeOrigin": true,
 		"pathRewrite": {
-			"^/api": ""
+			"^/api/contentservices": ""
 		}
 	},
-	"/gateway/api": {
-		"target": "http://0.0.0.0:7777/gateway/api",
+	"/gateway/api/contentservices": {
+		"target": "http://0.0.0.0:7777/gateway/api/contentservices",
 		"secure": false,
 		"changeOrigin": true,
 		"pathRewrite": {
-			"^/api": ""
+			"^/gateway/api/contentservices": ""
 		}
-	}
+	},
+	"*": {
+    target: "http://0.0.0.0:3003/",
+    pathRewrite: (path, req) => {
+      console.log("path", path);
+      if (path.indexOf("?") < 0) {
+        path = path + "?" + "projectName=" + PROJECT_NAME;
+      } else {
+        path = path + "&" + "projectName=" + PROJECT_NAME;
+      }
+      return path;
+    },
+    bypass: (req, res, proxyOptions) => {
+      console.log("bypass", req.url);
+      if (req.headers.accept.indexOf("text/html") >= 0) {
+        return "/index.html";
+      }
+    },
+    secure: false,
+    logLevel: "debug",
+    changeOrigin: true
+  }
 }
 
 module.exports = PROXY_CONFIG;
